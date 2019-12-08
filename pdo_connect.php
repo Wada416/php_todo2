0878321936c6229c933ba51131cd667454b5c020
Wada416 <wada1982s@gmail.com>
2019-12-08 23:47:33 +0900

    db接続テスト　接続成功

--- pdo_connect.php
+++ pdo_connect.php
@@ -1,3 +1,12 @@
 <?php
-
+    $dsn = 'mysql:dbname=todo;host=127.0.0.1';
+    $user = 'homestead';
+    $password = 'secret';
+    try {
+        $dbh = new PDO($dsn, $user, $password);
+        echo "接続成功\n";
+    } catch (PDOException $e) {
+        echo "接続失敗: " . $e->getMessage() . "\n";
+        exit();
+    }
 ?>
