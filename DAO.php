bab4383a6948f652dd0fbf118eb1cd44759902ca
Wada416 <wada1982s@gmail.com>
2019-12-09 21:47:23 +0900

    function pdo　DB接続用関数

--- DAO.php
+++ DAO.php
@@ -1,3 +1,23 @@
 <?php
+    class DAO {
+        const dsn = 'mysql:dbname=todo;host=127.0.0.1';
+        const user = 'homestead';
+        const password = 'secret';
 
+        function pdo(){
+            $dsn = self::dsn;
+            $user = self::user;
+            $password = self::password;
+
+            try {
+                $pdo = new PDO($dsn, $user, $password);
+            } catch (PDOException $e) {
+                echo "接続失敗: " . $e->getMessage() . "\n";
+                exit();
+            }
+            return $pdo;
+        }
+
+
+    }
 ?>
