ff5514a907cbaec1c2b3de4804ba7a34d8a6835c
Wada416 <wada1982s@gmail.com>
2019-12-11 12:58:46 +0900

    ログインエラー時のリダイレクト処理　動作ok

--- login_logic.php
+++ login_logic.php
@@ -4,8 +4,10 @@
     $pass = $_POST['pass'];
     $dao = new DAO;
     $data = $dao->findUser($id,$pass);
-    echo $data['name'];
+    //echo $data['name'];
     if ($data == null){
-        echo "null";
+        session_start();
+        $_SESSION['msg'] = "ユーザーIDまたはパスワードが正しくありません";
+        header('Location: http://localhost/php_todo2/error.php');
     }
 ?>
