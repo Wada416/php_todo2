0c1fddcfd1f2dd4b9522e357aad7c2cebecb77d9
Wada416 <wada1982s@gmail.com>
2019-12-11 12:55:19 +0900

    error.php  htmlモックのエラー画面をコピー

---
+++ error.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>エラー</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <h1>エラー</h1>
+    <P>エラーが発生しました</P>
+    <p>「エラーメッセージ」</p>
+    <form action="">
+        <input type="submit" value="戻る"/>
+    </form>
+  </body>
+</html>
