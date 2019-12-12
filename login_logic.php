db56f92b0b63221681b946fe05de0348b8d77633
Wada416 <wada1982s@gmail.com>
2019-12-12 23:22:00 +0900

    分岐条件を変更　ユーザーが見つかればindex.phpへ、ユーザーが見つからなければerror.phpへ

--- login_logic.php
+++ login_logic.php
@@ -3,8 +3,10 @@
     session_start();
     $dao = new DAO;
     $data = $dao->findUser($_POST['id'],$_POST['pass']);
-    //echo $data['name'];
-    if ($data == null){
+
+    if ($data != null){
+        header('Location: http://localhost/php_todo2/index.php');
+    }else{
         $_SESSION['msg'] = "ユーザーIDまたはパスワードが正しくありません";
         header('Location: http://localhost/php_todo2/error.php');
     }
