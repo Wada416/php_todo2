61f921e56903ca4e41acee4399233ee77314b96e
Wada416 <wada1982s@gmail.com>
2019-12-16 23:38:42 +0900

    HTMLモックをコピーしてregist.phpを作成

---
+++ regist.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+    <html lang="ja"">
+<head>
+    <meta charset="utf-8">
+    <title>登録</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <h1>登録</h1>
+    <form action="">
+        担当者:
+        <select name="user_name">
+            <option value="">aaa</option>
+            <option value="">bbb</option>
+            <option value="">ccc</option>
+        </select>
+        <br/>
+
+        作業名称:
+        <input tyep="text" name="work_name">
+        <br/>
+
+        期限:
+        <input type="date" name="deadline">
+        <br/>
+
+        <input type="submit" value="登録">
+    </form>
+</body>
+
+</html>
