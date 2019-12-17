c7fb28a00adb724a8154b833a1e0702e588ab2fd
Wada416 <wada1982s@gmail.com>
2019-12-18 00:02:45 +0900

    sessin_startを追加、アイテムリストを再取得してセッションに保存しindex.phpへリダイレクト

--- regist_logic.php
+++ regist_logic.php
@@ -1,5 +1,9 @@
 <?php
     require "DAO.php";
+    session_start();
     $dao = new DAO;
-    $dao->registItem($_POST)
+    $dao->registItem($_POST);
+    $itemList = $dao->getItemList();
+    $_SESSION['itemList'] = $itemList;
+    header('Location: http://localhost/php_todo2/index.php');
 ?>
