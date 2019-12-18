e458e4eb772fa289c6acafcc6180efebb891883f
Wada416 <wada1982s@gmail.com>
2019-12-18 23:09:49 +0900

    htmlモックをコピーしてedit.phpを作成

---
+++ edit.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+    <html lang="ja"">
+<head>
+    <meta charset="utf-8">
+    <title>更新</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <h1>更新</h1>
+    <form action="">
+        担当者:
+        <select name="user_name">
+            <option value="">aaa</option>
+            <option value="">bbb</option>
+            <option value="">ccc</option>
+        </select>
+        <br/>
+
+        作業名称:
+        <input tyep="text" name="work_name">
+        <br/>
+
+        期限:
+        <input type="date" name="deadline">
+        <br/>
+
+        <input type="submit" value="更新">
+    </form>
+</body>
+
+</html>
