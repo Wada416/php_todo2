8d42e5d3987a20060d440af2e6cf42912873cc7a
Wada416 <wada1982s@gmail.com>
2019-12-19 00:02:59 +0900

    更新の入力ホームに初期値を表示する

--- edit.php
+++ edit.php
@@ -1,5 +1,12 @@
+<?php
+    $item = $_POST['item'];
+    $name = $item['name'];
+    $user = $item['user'];
+    $deadline = $item['deadline'];
+?>
+
 <!DOCTYPE html>
-    <html lang="ja"">
+    <html lang="ja">
 <head>
     <meta charset="utf-8">
     <title>更新</title>
@@ -10,20 +17,19 @@
 <body>
     <h1>更新</h1>
     <form action="">
+        作業名称:
+        <input tyep="text" name="work_name" value="<?php echo $name; ?>">
+        <br/>
         担当者:
         <select name="user_name">
+            <option value="" selected><?php echo $user; ?></option>
             <option value="">aaa</option>
             <option value="">bbb</option>
             <option value="">ccc</option>
         </select>
         <br/>
-
-        作業名称:
-        <input tyep="text" name="work_name">
-        <br/>
-
         期限:
-        <input type="date" name="deadline">
+        <input type="date" name="deadline" value="<?php echo $deadline; ?>">
         <br/>
 
         <input type="submit" value="更新">
