c1b56b4847cfa2995b2c6f64fb48a547e4939a4b
Wada416 <wada1982s@gmail.com>
2019-12-22 18:23:57 +0900

    引数を修正

--- delete_logic.php
+++ delete_logic.php
@@ -2,7 +2,7 @@
     require "DAO.php";
     session_start();
     $dao = new DAO;
-    $dao->deleteItem($_POST);
+    $dao->deleteItem($_POST['id']);
     $itemList = $dao->getItemList();
     $_SESSION['itemList'] = $itemList;
     header('Location: http://localhost/php_todo2/index.php');
