722bb6147fbb18d54eb5e2dadf76505516aa55d5
Wada416 <wada1982s@gmail.com>
2019-12-22 18:03:47 +0900

    htmlモックをコピーしてdelete.phpを作成

---
+++ delete.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+    <html lang="ja"">
+<head>
+    <meta charset="utf-8">
+    <title>削除</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <h1>削除</h1>
+    項目を削除しますか？
+    <br/>
+
+    <form action="">
+        <input type="submit" value="削除">
+    </form>
+
+    <form action="">
+            <input type="submit" value="キャンセル">
+    </form>
+
+</body>
+
+</html>
