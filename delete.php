22a7bc4ba34fc05bbe600070aef942a5cc403fb1
Wada416 <wada1982s@gmail.com>
2019-12-22 18:14:53 +0900

    削除対象の項目の内容を表示、削除ボタンのリンク先をdelete_logic.phpに

--- delete.php
+++ delete.php
@@ -9,11 +9,35 @@
 
 <body>
     <h1>削除</h1>
-    項目を削除しますか？
+    次の項目を削除しますか？
     <br/>
 
-    <form action="">
+    <table border="1">
+      <tr>
+        <th>作業名称</th>
+        <th>担当者</th>
+        <th>期限</th>
+        <th>終了日</th>
+      </tr>
+      <tr>
+        <td>
+          <?php echo $_POST['name'] ?>
+        </td>
+        <td>
+          <?php echo $_POST['user'] ?>
+        </td>
+        <td>
+          <?php echo $_POST['deadline'] ?>
+        </td>
+        <td>
+          <?php echo $_POST['end_date'] ?>
+        </td>
+      </tr>
+    </table>
+
+    <form method="post" action="delete_logic.php">
         <input type="submit" value="削除">
+        <input type="hidden" name="id" value="<?php echo $_POST['id']; ?>">
     </form>
 
     <form action="">
